23cb14708843e51f30a84b1e15f589a2a03d0ed2
liguowei <guowei400@gmail.com>
2016-01-01 11:18:27 +0800

    添加注释

--- app/src/main/java/com/lgw/androidutils/LogUtil.java
+++ app/src/main/java/com/lgw/androidutils/LogUtil.java
@@ -3,7 +3,8 @@ package com.lgw.androidutils;
 import android.util.Log;
 
 /**
- * Created by lgw on 2016-01-01.
+ * Created by lgw on 2016-01-01
+ * 调整LEVEL控制日志打印
  */
 public class LogUtil {
     public static final int VERBOSE = 1;
@@ -14,30 +15,60 @@ public class LogUtil {
     public static final int NOTHING = 6;
     public static final int LEVEL = VERBOSE;
 
+    /**
+     * verbose级别日志打印
+     *
+     * @param tag 标签,信息头
+     * @param msg 要打印的信息
+     */
     public static void v(String tag, String msg) {
         if (LEVEL <= VERBOSE) {
             Log.v(tag, msg);
         }
     }
 
+    /**
+     * debug级别日志打印
+     *
+     * @param tag 标签,信息头
+     * @param msg 要打印的信息
+     */
     public static void d(String tag, String msg) {
         if (LEVEL <= DEBUG) {
             Log.d(tag, msg);
         }
     }
 
+    /**
+     * info级别日志打印
+     *
+     * @param tag 标签,信息头
+     * @param msg 要打印的信息
+     */
     public static void i(String tag, String msg) {
         if (LEVEL <= INFO) {
             Log.i(tag, msg);
         }
     }
 
+    /**
+     * warn级别日志打印
+     *
+     * @param tag 标签,信息头
+     * @param msg 要打印的信息
+     */
     public static void w(String tag, String msg) {
         if (LEVEL <= WARN) {
             Log.w(tag, msg);
         }
     }
 
+    /**
+     * error级别日志打印
+     *
+     * @param tag 标签,信息头
+     * @param msg 要打印的信息
+     */
     public static void e(String tag, String msg) {
         if (LEVEL <= ERROR) {
             Log.e(tag, msg);
